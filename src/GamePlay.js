18dd3425907ee59863433c16d63d0d5f0d9b7419
Nahum Okysoft <nahum.poar@gmail.com>
2019-04-08 13:50:30 -0500

    Loading images and showing it on screen

--- src/GamePlay.js
+++ src/GamePlay.js
@@ -3,18 +3,20 @@ GamePlayManager = {
     init: function(){
         console.log("init")
     },
-    preload: function(){
-        console.log("preload")
+    preload: function(){ //Cargue nuestras imagenes
+        game.load.image('background', 'assets/img/background.png');
+        game.load.image('wood_cutter', 'assets/img/man_stand.png');
     },
     create: function(){
-        console.log("create")
+        game.add.sprite(0,0, 'background');
+        game.add.sprite(50,50, 'wood_cutter');
     },
     update: function(){
         console.log("update")
     }
 }
 
-var game =  new Phaser.Game(320, 480, Phaser.AUTO);
+var game =  new Phaser.Game(320, 480, Phaser.CANVAS);
 
 game.state.add("gameplay", GamePlayManager);
 game.state.start("gameplay");
